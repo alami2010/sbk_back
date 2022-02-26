d6b8008efa11695a7cf027424227c7f1810fe676
Youssef Dahar <youssef.dahar@gmail.com>
2022-02-26 12:56:02 +0100

    add aws database and clean test

---
+++ src/main/java/com/ydahar/sbk/web/rest/HelloResource.java
@@ -0,0 +1,31 @@
+package com.ydahar.sbk.web.rest;
+
+import com.ydahar.sbk.domain.City;
+import com.ydahar.sbk.repository.CityRepository;
+import com.ydahar.sbk.service.CityService;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.jhipster.web.util.ResponseUtil;
+
+/**
+ * REST controller for managing {@link City}.
+ */
+@RestController
+@RequestMapping("/")
+public class HelloResource {
+
+    private final Logger log = LoggerFactory.getLogger(HelloResource.class);
+
+    @GetMapping("/")
+    public String hello() {
+        log.debug("REST request to hello world");
+        return "hello world";
+    }
+}
